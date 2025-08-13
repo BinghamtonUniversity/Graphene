afe06f3e90fee7fca20bdcf83b0e4327596f2d9d
Tim Cortesi <tcortesi@gmail.com>
2025-08-12 20:00:59 -0400

    Add REDIS_SCHEME

--- config/database.php
+++ config/database.php
@@ -140,6 +140,7 @@ return [
         ],
 
         'default' => [
+            'scheme' => env('REDIS_SCHEME', 'tcp'), // Use 'tls' for AWS ElastiCache
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
