ef724ca2a51b1500b1d677e68af5e82629acc044
Tim Cortesi <tcortesi@gmail.com>
2026-01-13 11:41:13 -0500

    Add AWS SES Tenant Support

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -16,6 +16,9 @@ class EventServiceProvider extends ServiceProvider
         'App\Events\UpdateUser' => [
             'App\Listeners\InvalidateUserCache',
         ],
+        \Illuminate\Mail\Events\MessageSending::class => [
+            \App\Listeners\AddSesTenantHeader::class,
+        ],
     ];
 
     /**
