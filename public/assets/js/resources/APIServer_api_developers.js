62b32bb44d368527e92ce4d41eb9ecb4f157e351
Ali Kemal Tanriverdi <11221504+alikemaltanriverdi@users.noreply.github.com>
2026-08-27 09:38:38 -0400

    bug fix for developer add has been fixed

--- public/assets/js/resources/APIServer_api_developers.js
+++ public/assets/js/resources/APIServer_api_developers.js
@@ -4,34 +4,41 @@ url = "/api/proxy/"+slug+"/apis/"+resource_id+"/developers";
 $.ajax({
   url: url,
   success: function (data) {
-    tableConfig.schema = [
+    custom_config = {}
+    custom_config.el = '#table'
+    custom_config.schema = [
       {name: 'id', type:'hidden'},
       {label: 'Developer', name:'user_id', required: true,type:'select',options:'/api/proxy/'+slug+'/users',format:{label:"{{name}}",value:function(item){return item.id;}}},
     ];
-    tableConfig.actions = [
-      { name: "create" },
+    custom_config.actions = [
+      { name: "create" ,label:"New",type:"success"},
       "|",
-      { name: "delete" },
+      { name: "delete",label:"Delete",type:"danger" },
     ];
-
-    tableConfig.data = data;
-    tableConfig.name = "api_developers";
-    grid = new GrapheneDataGrid(tableConfig);
-    grid
-        .on("model:created", function (grid_event) {
-          grid_event.preventDefault()
-          $.ajax({
-            url: url + "/" + grid_event.model.attributes.user_id,
-            type: "POST",
-            data: grid_event.model.attributes
-          });
-        }).on("model:deleted",function(grid_event){
-          grid_event.preventDefault()
-          $.ajax({
-            url: url + "/" + grid_event.model.attributes.user_id,
-            type: "DELETE",
-            data: grid_event.model.attributes
-          });
-        })
+    custom_config.data = data;
+    custom_config.name = "api_developers";
+    grid = new GrapheneDataGrid(custom_config);
+    grid.on("model:created", function (grid_event) {
+        $.ajax({
+          url: url + "/" + grid_event.model.attributes.user_id,
+          type: "POST",
+          success: function (data) {
+          toastr.success("Successfully added the developer!")
+          },
+          error: function (e) {
+            toastr.error(e.statusText, "ERROR");
+          },
+        });
+    }).on("model:deleted",function(grid_event){
+            grid_event.preventDefault()
+            $.ajax({
+              url: url + "/" + grid_event.model.attributes.user_id,
+              type: "DELETE",
+              data: grid_event.model.attributes,
+              success: function (data) {
+                toastr.success("Developer removed!")
+              }
+            });
+      })
   },
 });
